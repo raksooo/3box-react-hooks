f89a4c4c589662cb096606c536b765e17732c092
Oskar Nyberg <oskar@oskarnyberg.com>
2019-05-25 18:39:00 +0200

    Refactored tests

--- test/3boxHooks.test.js
+++ test/3boxHooks.test.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { configure } from 'enzyme';
 import chai, { expect } from 'chai';
 import FakeProvider from 'web3-fake-provider';
 import {
   useProfile,
   useBox,
-  useDelayedBox,
   useSpace,
+  useDelayedBox,
   useDelayedSpace
 } from '../src/3boxHooks.js';
-import { asyncHookPromise } from './testHelper';
+import { testHook, testHookN } from './testHelper';
 import 'jsdom-global/register';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+const TEST_PROFILE = {
+  address: '0x88E146E0fd0F5AaCbc4f94365dF9f599A90139F1',
+  name: 'Oskar',
+};
+const TEST_SPACE = '3box-react-hooks-test-space';
+
 describe('Ethereum specific hooks', function() {
   this.timeout(5000);
-
-  const TEST_PROFILE = {
-    address: '0x88E146E0fd0F5AaCbc4f94365dF9f599A90139F1',
-    name: 'Oskar',
-  };
-  const TEST_SPACE = '3box-react-hooks-test-space';
 
   const provider = new FakeProvider();
 
@@ -31,49 +29,29 @@ describe('Ethereum specific hooks', function() {
     provider.injectResult('0x0');
   });
 
-  it('should retrieve a profile', function() {
-    const Component = ({ resolve }) => {
-      const profile = useProfile(TEST_PROFILE.address);
-      profile != null && resolve(profile);
-      return null;
-    }
-    const promise = asyncHookPromise(Component);
+  it('should retrieve a profile', async function() {
+    const profile = await testHook(useProfile, TEST_PROFILE.address);
 
-    return promise.then(profile => {
-      expect(profile).to.not.be.null;
-      expect(profile).to.have.property('name', TEST_PROFILE.name);
-    });
+    expect(profile).to.not.be.null;
+    expect(profile).to.have.property('name', TEST_PROFILE.name);
   });
 
-  it('should retrieve a box', function() {
-    const Component = ({ resolve }) => {
-      const box = useBox(TEST_PROFILE.address, provider);
-      box != null && resolve(box);
-      return null;
-    }
-    const promise = asyncHookPromise(Component);
+  it('should retrieve a box', async function() {
+    const box = await testHook(useBox, TEST_PROFILE.address, provider);
 
-    return promise.then(box => {
-      expect(box).to.not.be.null;
-      expect(box).to.have.property('private');
-      expect(box).to.have.property('public');
-      expect(box).to.have.property('spaces');
-    });
+    expect(box).to.not.be.null;
+    expect(box).to.have.property('private');
+    expect(box).to.have.property('public');
+    expect(box).to.have.property('spaces');
   });
 
-  it('should retrieve a space', function() {
-    const Component = ({ resolve }) => {
-      const [space] = useSpace(TEST_SPACE, TEST_PROFILE.address, provider);
-      space != null && resolve(space);
-      return null;
-    }
-    const promise = asyncHookPromise(Component);
+  it('should retrieve a space', async function() {
+    const space = await testHookN(useSpace, 0, TEST_SPACE, TEST_PROFILE.address, provider);
 
-    return promise.then(space => {
-      expect(space).to.not.be.null;
-      expect(space).to.have.property('private');
-      expect(space).to.have.property('public');
-    });
+    expect(space).to.not.be.null;
+    expect(space).to.have.property('private');
+    expect(space).to.have.property('public');
+    expect(space).to.have.property('_name', TEST_SPACE);
   });
 });
 
--- test/testHelper.js
+++ test/testHelper.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
+export const testHook = (hook, ...args) => {
+  return testHookN(hook, null, ...args);
+};
+
+export const testHookN = (hook, n, ...args) => {
+  const Component = ({ resolve }) => {
+    let result = hook(...args);
+    result = n == null ? result : result[n];
+    result != null && resolve(result);
+    return null;
+  }
+  return asyncHookPromise(Component);
+};
+
 export const asyncHookPromise = Component => {
   return new Promise(resolve => {
     mount(<Component resolve={resolve} />);
